a8407b8e9b2e7f5886e57bd2d7eee5b699ac49de
monapink <91607167+antfightclub@users.noreply.github.com>
2024-08-19 17:30:22 +0200

    it is back asswards doe

--- Assets/EventCreator.cs
+++ Assets/EventCreator.cs
@@ -19,7 +19,7 @@ namespace EventQueue
             if (Input.GetMouseButtonDown(0))
             {
                 float distanceToPlayer = ObserverEventQueue.GetDistanceToPlayer(this.transform.position);
-                EventQueueManager.Instance.AddEventToQueue(new MessageEvent("Hiii :3 I said hiii :3 at " + System.DateTime.Now + ", and the distance to player is " + distanceToPlayer, distanceToPlayer));
+                EventQueueManager.Instance.AddEventToQueue(new MessageEvent("Hiii :3 I said hiii :3 at " + System.DateTime.Now + ", and my distance to player is " + distanceToPlayer + " light seconds!", distanceToPlayer));
             }
         }
 

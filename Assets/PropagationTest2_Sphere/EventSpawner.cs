f65493a382cd6fc6571d34a110ddc313386b6231
monapink <91607167+antfightclub@users.noreply.github.com>
2024-08-26 13:55:53 +0200

    messing

---
+++ Assets/PropagationTest2_Sphere/EventSpawner.cs
@@ -0,0 +1,21 @@
+using EventQueue;
+using UnityEngine;
+
+public class EventSpawner : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+
+    }
+}

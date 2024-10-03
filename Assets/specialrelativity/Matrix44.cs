3d994263309caf22ed3291cd360e551253b48b1b
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-03 20:37:45 +0200

    Implement norm and products

--- Assets/specialrelativity/Matrix44.cs
+++ Assets/specialrelativity/Matrix44.cs
@@ -3,19 +3,33 @@ using UnityEngine;
 namespace SpecialRelativity
 {
     public class Matrix44
-    {
-        
-        Vector3 innerProduct(Vector3 x, Vector3 y)
+    { 
+        Vector3 innerProduct(Vector3 a, Vector3 b)
         {
-
+            return new Vector3(
+                a.x * b.x, 
+                a.y * b.y, 
+                a.z * b.z);
         } 
 
-        Vector3 lorentzianInnerProduct(Vector3 x, Vector3 y)
+        Vector3 lorentzianInnerProduct(Vector4 a, Vector4 b)
         {
-
-
-
+            return new Vector4(
+              -(a.w * b.w), 
+                a.x * b.x,
+                a.y * b.y,
+                a.z * b.z);
         }
+
+        Vector4 lorentzianNorm(Vector4 a)
+        {
+            return new Vector4(
+              -(a.w * a.w),
+                a.x * a.x,
+                a.y * a.y,
+                a.z * a.z);
+        }
+        
 
 
 

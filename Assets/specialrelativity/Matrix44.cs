203b25606512373a313943709617e31f59f254ba
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-03 20:11:53 +0200

    class def for product operation
    normal inner product and lorentzian inner product

--- Assets/specialrelativity/Matrix44.cs
+++ Assets/specialrelativity/Matrix44.cs
@@ -4,6 +4,20 @@ namespace SpecialRelativity
 {
     public class Matrix44
     {
+        
+        Vector3 innerProduct(Vector3 x, Vector3 y)
+        {
+
+        } 
+
+        Vector3 lorentzianInnerProduct(Vector3 x, Vector3 y)
+        {
+
+
+
+        }
+
+
 
     }
 

e2421beeb21ae49dcea4ba081228461972760545
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-03 21:01:31 +0200

    separate lorentz and matrix44

---
+++ Assets/specialrelativity/Lorentz.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace SpecialRelativity
+{
+    public class Lorentz
+    {
+        Vector3 innerProduct(Vector3 a, Vector3 b)
+        {
+            return new Vector3(
+                a.x * b.x,
+                a.y * b.y,
+                a.z * b.z);
+        }
+
+        Vector3 lorentzianInnerProduct(Vector4 a, Vector4 b)
+        {
+            return new Vector4(
+              -(a.w * b.w),
+                a.x * b.x,
+                a.y * b.y,
+                a.z * b.z);
+        }
+
+        Vector4 lorentzianNorm(Vector4 a)
+        {
+            return new Vector4(
+              -(a.w * a.w),
+                a.x * a.x,
+                a.y * a.y,
+                a.z * a.z);
+        }
+    }
+
+}

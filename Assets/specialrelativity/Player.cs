7c7f0811bff44d0653f4725c0f6ecccc90a419c5
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-04 17:50:37 +0200

    Tentative impl on Player and World

--- Assets/specialrelativity/Player.cs
+++ Assets/specialrelativity/Player.cs
@@ -5,8 +5,13 @@ namespace SpecialRelativity
 {
     public class Player
     {
-        
-        void Init()
+        World world;
+        PhaseSpace P;
+        Quaternion Quaternion;
+        Vector4 Position;
+        double time;
+
+        void Init(World world, Vector4 position)
         {
 
         }

42f228d7bee8f2506f84504451bdab2e9bc7bc61
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-05 16:51:24 +0200

    Implement vector4D

--- Assets/specialrelativity/Math/Vector4D.cs
+++ Assets/specialrelativity/Math/Vector4D.cs
@@ -98,33 +98,43 @@ namespace SpecialRelativity
             return this.x * this.x + this.y * this.y + this.z * this.z;
         }
 
-        public double DistanceTo()
+        
+        public double DistanceTo(Vector4D v)
         {
+            double x, y, z;
+            x = this.x - v.x;
+            y = this.y - v.y;
+            z = this.z - v.z;
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public double DistanceToSquared(Vector4D v)
+        {
+            double x, y, z;
+            x = this.x - v.x;
+            y = this.y - v.y;
+            z = this.z - v.z;
+            return x * x + y * y + z*z;
 
         }
 
-        public double DistanceToSquared()
+        public double Dot(Vector4D v)
         {
-
-        }
-
-        public double Dot()
-        {
-
+            return this.x * v.x + this.y * v.y + this.z * v.z;
         }
 
         public double GetGamma()
         {
-
+            return Math.Sqrt(1.0d + this.x * this.x + this.y * this.y + this.z * this.z);
         }
 
         /// <summary>
         /// Lorentzian inner product!
         /// </summary>
         /// <returns></returns>
-        public double InnerProduct()
+        public double InnerProduct(Vector4D v)
         {
-
+            return Math.Sqrt(this.x * v.x + this.y * v.y + this.z * v.z - this.t * v.t);
         }
         /// <summary>
         /// Lorentzian squared norm
@@ -132,32 +142,70 @@ namespace SpecialRelativity
         /// <returns></returns>
         public double SquaredNorm()
         {
-
+            return this.x * this.x + this.y * this.y + this.z * this.z - this.t * this.t;
         }
 
         public double SquaredNormTo(Vector4D v)
         {
-
+            double t, x, y, z;
+            t = this.t - v.t;
+            x = this.x - v.x;
+            y = this.y - v.y;
+            z = this.z - v.z;
+            return x * x + y * y + z * z - t * t;
         }
 
-        public double Hat()
+        public Vector3D Hat()
         {
-
+            double r = this.x * this.x + this.y * this.y + this.z * this.z;
+            if (r.Equals8DigitPrecision(0.0d))
+            {
+                return new Vector3D(1.0d, 0.0d, 0.0d);
+            }
+            r = 1.0d / Math.Sqrt(r);
+            return new Vector3D(this.x * r, this.y * r, this.z * r);
         }
 
-        public double Normalize()
+        public Vector4D Normalize()
         {
-
+            double r = this.x * this.x + this.y * this.y + this.z * this.z;
+            if (r.Equals8DigitPrecision(0.0d))
+            {
+                return new Vector4D(this.t, 0.0d, 0.0d, 0.0d);
+            }
+            r = 1.0d / Math.Sqrt(r);
+            return new Vector4D(this.t, this.x*r, this.y*r, this.z*r);
         }
 
         public Vector4D GetLinearAdd(Vector4D N, double s)
         {
-
+            double tt, xx, yy, zz;
+            tt = this.t + N.t * s;
+            xx = this.x + N.x * s;
+            yy = this.y + N.y * s;
+            zz = this.z + N.z * s;
+            return new Vector4D(tt, xx, yy, zz);
         }
 
-        public Vector4 GetDivPoint(Vector4D v, double s)
+        /// <summary>
+        /// s must be between 0 and 1
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public Vector4D GetDivPoint(Vector4D v, double s)
         {
-
+            if(s>0 && s<1)
+            {
+                throw new ArgumentOutOfRangeException("s must lie in the interval [0 and 1]");
+            }
+            double t, tt, xx, yy, zz;
+            t  = 1.0d - s;
+            tt = this.t * t + v.t * s;
+            xx = this.x * t + v.x * s;
+            yy = this.y * t * v.y * s;
+            zz = this.z * t * v.z * s;
+            return new Vector4D(tt, xx, yy, zz);
         }
 
     }

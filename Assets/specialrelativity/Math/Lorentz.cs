a48d3f83df27b97834207a5469e426fd3294502d
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-06 12:18:36 +0200

    lorentz.cs double and types

--- Assets/specialrelativity/Math/Lorentz.cs
+++ Assets/specialrelativity/Math/Lorentz.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
+using System;
 
 namespace SpecialRelativity
 {
@@ -9,32 +10,32 @@ namespace SpecialRelativity
         // 1 second = 299792458 meters
         // if c is given as natural units, velocity should be given as fraction of 1
         // if c is given as lightspeed in meters per second, velocity should be given as meters per second
-        public int c_natural = 1; 
-        public int c_lightspeed = 299792458; // lightspeed is defined as meters per second!
+        public double c_natural = 1.0d; 
+        public double c_lightspeed = 299792458.0d; // lightspeed is defined as meters per second!
 
         // Returns the ordinary inner product of two 3-vectors
-        Vector3 InnerProduct(Vector3 a, Vector3 b)
+        Vector3D InnerProduct(Vector3D a, Vector3D b)
         {
-            return new Vector3(
+            return new Vector3D(
                 a.x * b.x,
                 a.y * b.y,
                 a.z * b.z);
         }
 
         // Returns the Lorentzian inner product of two 4-vectors
-        // Note that I'm using wxyz
-        Vector3 LorentzianInnerProduct(Vector4 a, Vector4 b)
+        // Note that I'm using txyz
+        Vector4D LorentzianInnerProduct(Vector4D a, Vector4D b)
         {
-            return new Vector4(
-              -(a.w * b.w),
+            return new Vector4D(
+              -(a.t * b.t),
                 a.x * b.x,
                 a.y * b.y,
                 a.z * b.z);
         }
 
-        float OrdinarySquaredNorm(Vector3 a)
+        double OrdinarySquaredNorm(Vector3D a)
         {
-            float norm = 
+            double norm = 
                   a.x * a.x
                 + a.y * a.y
                 + a.z * a.z;
@@ -43,9 +44,9 @@ namespace SpecialRelativity
 
         // Returns the Lorentzian Norm, which tells us whether a 4-vector in D-dimensional spacetime
         // is spacelike, lightlike, or timelike compared to the rest frame (origin)
-        float LorentzianNorm(Vector4 a)
+        double LorentzianNorm(Vector4D a)
         {
-            float res = (-(a.w * a.w) 
+            double res = (-(a.t * a.t) 
                 + a.x * a.x 
                 + a.y * a.y 
                 + a.z * a.z);
@@ -68,9 +69,9 @@ namespace SpecialRelativity
         /// </summary>
         /// <param name="a"></param>
         /// <returns>Likeness enum</returns>
-        public Likeness DetermineLikeness(Vector4 a)
+        public Likeness DetermineLikeness(Vector4D a)
         {
-            float lorentzianNorm = LorentzianNorm(a);
+            double lorentzianNorm = LorentzianNorm(a);
             if(lorentzianNorm >  0) { return Likeness.SpaceLike; }
             if(lorentzianNorm == 0) { return Likeness.LightLike; }
             if(lorentzianNorm <  0) { return Likeness.TimeLike;  }
@@ -78,85 +79,85 @@ namespace SpecialRelativity
         }
 
         // next implement boost matrix and rotation matrix
-        public Matrix44 LorentzBoostMatrix(Vector3 vel)
+        public Matrix44 LorentzBoostMatrix(Vector3D vel)
         {
-            float beta_x = vel.x / c_lightspeed;
-            float beta_y = vel.y / c_lightspeed;
-            float beta_z = vel.z / c_lightspeed;
-            float v_2 = (beta_x * beta_x + beta_y * beta_y + beta_z * beta_z);
-            float gamma = 1 / (Mathf.Sqrt(1 - v_2));
+            double beta_x = vel.x / c_lightspeed;
+            double beta_y = vel.y / c_lightspeed;
+            double beta_z = vel.z / c_lightspeed;
+            double v_2 = (beta_x * beta_x + beta_y * beta_y + beta_z * beta_z);
+            double gamma = 1 / (Math.Sqrt(1.0d - v_2));
             return new Matrix44(
-                new Vector4(gamma,         -beta_x* gamma,                  -beta_y*gamma,                  -beta_z*gamma                    ),
-                new Vector4(-beta_x*gamma, (gamma-1)*((vel.x*vel.x)/v_2)+1, (gamma-1)*((vel.y*vel.x)/v_2)  , (gamma-1)*((vel.z*vel.x)/v_2)   ),
-                new Vector4(-beta_y*gamma, (gamma-1)*((vel.x*vel.y)/v_2)  , (gamma-1)*((vel.y*vel.y)/v_2)+1, (gamma-1)*((vel.z*vel.y)/v_2)   ),
-                new Vector4(-beta_z*gamma, (gamma-1)*((vel.x*vel.z)/v_2)  , (gamma-1)*((vel.y*vel.z)/v_2)  , (gamma-1)*((vel.z*vel.z)/v_2)+1 ));
+                new Vector4D(gamma,         -beta_x* gamma,                  -beta_y*gamma,                  -beta_z*gamma                    ),
+                new Vector4D(-beta_x*gamma, (gamma-1)*((vel.x*vel.x)/v_2)+1, (gamma-1)*((vel.y*vel.x)/v_2)  , (gamma-1)*((vel.z*vel.x)/v_2)   ),
+                new Vector4D(-beta_y*gamma, (gamma-1)*((vel.x*vel.y)/v_2)  , (gamma-1)*((vel.y*vel.y)/v_2)+1, (gamma-1)*((vel.z*vel.y)/v_2)   ),
+                new Vector4D(-beta_z*gamma, (gamma-1)*((vel.x*vel.z)/v_2)  , (gamma-1)*((vel.y*vel.z)/v_2)  , (gamma-1)*((vel.z*vel.z)/v_2)+1 ));
 
         }
 
-        public Matrix44 RotMatrixR(Vector3 col1, Vector3 col2, Vector3 col3)
+        public Matrix44 RotMatrixR(Vector3D col1, Vector3D col2, Vector3D col3)
         {
             return new Matrix44(
-                new Vector4(1, 0, 0, 0),
-                new Vector4(0, col1.x, col1.y, col1.z),
-                new Vector4(0, col2.x, col2.y, col2.z),
-                new Vector4(0, col3.x, col3.y, col3.z));
+                new Vector4D(1, 0, 0, 0),
+                new Vector4D(0, col1.x, col1.y, col1.z),
+                new Vector4D(0, col2.x, col2.y, col2.z),
+                new Vector4D(0, col3.x, col3.y, col3.z));
         }
         
         // then proper time and lorentz covariant velocity
 
         // Returns the displacement D vector between current and previous position
-        public Vector4 DisplacementDVector(Vector4 previous, Vector4 current)
+        public Vector4D DisplacementDVector(Vector4D previous, Vector4D current)
         {
-            return new Vector4(
-                current.w - previous.w,
+            return new Vector4D(
+                current.t - previous.t,
                 current.x - previous.x,
                 current.y - previous.y,
                 current.z - previous.z);
         }
         // Returns the proper time increased for a particle by an amount delta from the displacementDvector
-        public float ProperTimeDelta(Vector4 DisplacementDVector)
+        public double ProperTimeDelta(Vector4D DisplacementDVector)
         {
-            float norm = LorentzianNorm(DisplacementDVector);
-            return Mathf.Sqrt(-norm);
+            double norm = LorentzianNorm(DisplacementDVector);
+            return Math.Sqrt(-norm);
         }
 
         // Unsure whether this actually works since on paper it involves a derivative (eq. 41, sogebu et al)
-        public Vector4 DVelocityVector(Vector4 DisplacementDVector, float properTimeDelta)
+        public Vector4D DVelocityVector(Vector4D DisplacementDVector, double properTimeDelta)
         {
-            return new Vector4(
-                DisplacementDVector.w / properTimeDelta,
+            return new Vector4D(
+                DisplacementDVector.t / properTimeDelta,
                 DisplacementDVector.x / properTimeDelta,
                 DisplacementDVector.y / properTimeDelta,
                 DisplacementDVector.z / properTimeDelta);
         }
 
-        public Vector4 AdvanceProperTimeStep(Vector4 current, Vector4 DisplacementDVector, float ProperTimeDelta)
+        public Vector4D AdvanceProperTimeStep(Vector4D current, Vector4D DisplacementDVector, double ProperTimeDelta)
         {
-            return new Vector4(
-                current.w + DisplacementDVector.w * ProperTimeDelta,
+            return new Vector4D(
+                current.t + DisplacementDVector.t * ProperTimeDelta,
                 current.x + DisplacementDVector.x * ProperTimeDelta,
                 current.y + DisplacementDVector.y * ProperTimeDelta,
                 current.z + DisplacementDVector.y * ProperTimeDelta);
         }
 
         // not sure if this works since on paper it involves a derivative (eq. 53, sogebu et al)
-        public Vector4 DAccelerationVector(Vector4 previous, Vector4 current, float properTimeDelta)
+        public Vector4D DAccelerationVector(Vector4D previous, Vector4D current, double properTimeDelta)
         {
-            Vector4 displacementDVector = DisplacementDVector(previous, current);
-            return new Vector4(
-                displacementDVector.w / properTimeDelta,
+            Vector4D displacementDVector = DisplacementDVector(previous, current);
+            return new Vector4D(
+                displacementDVector.t / properTimeDelta,
                 displacementDVector.x / properTimeDelta,
                 displacementDVector.y / properTimeDelta,
                 displacementDVector.z / properTimeDelta);
         }
         
-        public float LorentzGammaFactor(Vector4 vel)
+        public double LorentzGammaFactor(Vector4D vel)
         {
-            float beta_x = vel.x / c_lightspeed;
-            float beta_y = vel.y / c_lightspeed;
-            float beta_z = vel.z / c_lightspeed;
-            float v_2 = (beta_x * beta_x + beta_y * beta_y + beta_z * beta_z);
-            float gamma = 1 / (Mathf.Sqrt(1 - v_2));
+            double beta_x = vel.x / c_lightspeed;
+            double beta_y = vel.y / c_lightspeed;
+            double beta_z = vel.z / c_lightspeed;
+            double v_2 = (beta_x * beta_x + beta_y * beta_y + beta_z * beta_z);
+            double gamma = 1 / (Math.Sqrt(1 - v_2));
             return gamma;
         }
         

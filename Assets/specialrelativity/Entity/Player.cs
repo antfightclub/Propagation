ff0f7f6f194679197b264262f8d45dc85473c3b5
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-06 17:17:53 +0200

    Tentative init of Player

--- Assets/specialrelativity/Entity/Player.cs
+++ Assets/specialrelativity/Entity/Player.cs
@@ -5,15 +5,24 @@ namespace SpecialRelativity
 {
     public class Player
     {
-        World world;
+        World World;
         PhaseSpace P;
-        Quaternion Quaternion;
-        Vector4 Position;
-        double time;
+        Quat Quaternion;
+        Vector4D Position;
+        double Time;
+        WorldLine Worldline;
 
-        void Init(World world, Vector4 position)
+
+        void Init(World world, Vector4D pos)
         {
-
+            this.World = world;
+            this.P = new PhaseSpace(pos, new Vector4D(1.0d, 0.0d, 0.0d, 0.0d));
+            this.Quaternion = new Quat();
+            this.Time = 0.0d;
+            PhaseSpace start = this.P.Copy();
+            start.X.t = 0.0d;
+            this.Worldline = WorldLine(start);
+            this.Worldline.Add(this.P);
         }
 
 

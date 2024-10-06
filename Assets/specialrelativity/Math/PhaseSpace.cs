30dca349a46d81e3c3c445914a1ff7c8ac4a2268
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-06 12:23:08 +0200

    phasespace.cs update double and type

--- Assets/specialrelativity/Math/PhaseSpace.cs
+++ Assets/specialrelativity/Math/PhaseSpace.cs
@@ -1,35 +1,36 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using System;
 
 namespace SpecialRelativity
 {
     public class PhaseSpace
     {
-        public Vector4 X; public Vector4 U;
+        public Vector4D X; public Vector4D U;
 
-        void Init(Vector4 X, Vector4 U)
+        void Init(Vector4D X, Vector4D U)
         {
-            this.X = new Vector4(X.w, X.x, X.y, X.z);
-            this.U = new Vector4(U.w, U.x, U.y, U.z);
+            this.X = new Vector4D(X.t, X.x, X.y, X.z);
+            this.U = new Vector4D(U.t, U.x, U.y, U.z);
         }
 
-        public PhaseSpace(Vector4 X, Vector4 U)
+        public PhaseSpace(Vector4D X, Vector4D U)
         {
-            this.X = new Vector4(X.w, X.x, X.y, X.z);
-            this.U = new Vector4(U.w, U.x, U.y, U.z);
+            this.X = new Vector4D(X.t, X.x, X.y, X.z);
+            this.U = new Vector4D(U.t, U.x, U.y, U.z);
         }
-        public Vector4 GetX() { return this.X; }
-        public Vector4 GetU() { return this.U; }
+        public Vector4D GetX() { return this.X; }
+        public Vector4D GetU() { return this.U; }
 
         public PhaseSpace Copy()
         {
             return new PhaseSpace(this.X, this.U);
         }
 
-        public Vector4 GetResist(float b)
+        public Vector4D GetResist(double b)
         {
-            return new Vector4(
-                0.0f,
+            return new Vector4D(
+                0.0d,
                 -this.U.x * b,
                 -this.U.y * b,
                 -this.U.z * b);
@@ -37,33 +38,33 @@ namespace SpecialRelativity
 
         // I think I need to rethink some of the code from sogebu et al
         // Not sure... but I gotta fix the CS0120 error...
-        public Vector4 Transform(Vector4 acceleration, float ds)
+        public Vector4D Transform(Vector4D acceleration, double ds)
         {
-            this.X.w = this.X.w + this.U.w * ds;
+            this.X.t = this.X.t + this.U.t * ds;
             this.X.x = this.X.x + this.U.x * ds;
             this.X.y = this.X.y + this.U.y * ds;
             this.X.z = this.X.z + this.U.z * ds;
-            acceleration.w = 0.0f;
-            Vector4 neg = new Vector4(-U.w, -U.x, -U.y, -U.z);
+            acceleration.t = 0.0d;
+            Vector4D neg = new Vector4D(-U.t, -U.x, -U.y, -U.z);
             Matrix44 L = Matrix44.Zero;
             L = L.Lorentz(neg);
-            Vector4 accel = L.GetTransform(acceleration);
-            float r = accel.x * accel.x + accel.y * accel.y + accel.z * accel.z;
+            Vector4D accel = L.GetTransform(acceleration);
+            double r = accel.x * accel.x + accel.y * accel.y + accel.z * accel.z;
             if (r > 10.0)
             {
-                this.U += new Vector4(
-                accel.x * (ds * 10.0f / r),
-                accel.y * (ds * 10.0f / r),
-                accel.z * (ds * 10.0f / r));
+                this.U += new Vector4D(0.0d,
+                accel.x * (ds * 10.0d / r),
+                accel.y * (ds * 10.0d / r),
+                accel.z * (ds * 10.0d / r));
             }
             else
             {
-                this.U += new Vector4(
+                this.U += new Vector4D(0.0d,
                     accel.x * ds,
                     accel.y * ds,
                     accel.z * ds);
             }
-            this.U.w = Mathf.Sqrt(1.0f + this.U.x * this.U.x + this.U.y * this.U.y + this.U.z + this.U.z);
+            this.U.t = Math.Sqrt(1.0f + this.U.x * this.U.x + this.U.y * this.U.y + this.U.z + this.U.z);
             return this.U;
         }
     }

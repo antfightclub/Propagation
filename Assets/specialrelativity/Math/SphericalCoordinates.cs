1a740f45342973d4485935d4a26c3989cd931ebf
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-23 11:05:38 +0200

    Fix trigonometry

--- Assets/specialrelativity/Math/SphericalCoordinates.cs
+++ Assets/specialrelativity/Math/SphericalCoordinates.cs
@@ -9,19 +9,17 @@ namespace SpecialRelativity
     {
         public class Single
         {
-            float rho;
-            float theta;
-            float phi;
-
             public static float GetTheta(Vector3 v)
             {
-                return Mathf.Atan2(v.x, v.y) / (float)Constants.PI;
+                //return Mathf.Atan2(v.x, v.y) / (float)Constants.PI;
+                return Mathf.Atan2(Mathf.Sqrt(v.x * v.x + v.y * v.y), v.z);
             }
 
             public static float GetPhi(Vector3 v)
             {
-                var xzLen = new Vector2((float)v.x, (float)v.z).magnitude;
-                return Mathf.Atan2(-v.y, xzLen);
+                //var xzLen = new Vector2((float)v.x, (float)v.z).magnitude;
+                //return Mathf.Atan2(-v.y, xzLen);
+                return Mathf.Atan2(v.y, v.x);
             }
 
             public static float GetMagnitude(Vector3 v)
@@ -49,10 +47,10 @@ namespace SpecialRelativity
 
             public static Vector3 ConvertSphericalToRect(float rho, float theta, float phi)
             {
-                float r = GetR(rho, phi);
-                float x = r * Mathf.Cos(theta);
-                float y = r * Mathf.Sin(theta);
-                float z = rho * Mathf.Cos(phi);
+                //float r = GetR(rho, phi);
+                float x = rho * Mathf.Sin(theta) * Mathf.Cos(phi);
+                float y = rho * Mathf.Cos(theta);
+                float z = rho * Mathf.Sin(theta) * Mathf.Sin(phi);
                 return new Vector3(x, y, z);
             }
 
@@ -61,22 +59,17 @@ namespace SpecialRelativity
 
         public class Double
         {
-            public struct coords_double
-            {
-                double rho;
-                double theta;
-                double phi;
-            }
-
             public static double GetTheta(Vector3D v)
             {
-                return Math.Atan2(v.z, v.y) / Constants.PI; //originally atan2(v.x, v.z)
+                //return Math.Atan2(v.z, v.y) / Constants.PI; //originally atan2(v.x, v.z)
+                return Math.Atan2(Math.Sqrt(v.x*v.x + v.y*v.y), v.z);
             }
 
             public static double GetPhi(Vector3D v)
             {
-                var xzLen = new Vector2((float)v.x, (float)v.z).magnitude;
-                return Math.Atan2(-v.y, xzLen);
+                //var xzLen = new Vector2((float)v.x, (float)v.z).magnitude;
+                //return Math.Atan2(-v.y, xzLen);
+                return Math.Atan2(v.y, v.x);
             }
 
             public static double GetMagnitude(Vector3D v)
@@ -104,10 +97,11 @@ namespace SpecialRelativity
 
             public static Vector3D ConvertSphericalToRect(double rho, double theta, double phi)
             {
-                double r = GetR(rho, phi);
-                double x = r*Math.Cos(theta);
-                double y = r*Math.Sin(theta);
-                double z = rho * Math.Cos(phi);
+                //double r = GetR(rho, phi);
+                // swapped y and z for it to make sense with Unity's stuff, although it's still right-handed instead of unity's lefthanded.
+                double x = rho * Math.Sin(theta) * Math.Cos(phi);
+                double y = rho * Math.Cos(theta);
+                double z = rho * Math.Sin(phi) * Math.Sin(phi);
                 return new Vector3D(x, y, z);
             }
 

e9eb4f339d8dd0133e2fc6a27337ada58e986c01
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-26 22:55:00 +0200

    Add a preliminary SciptableObject

---
+++ Assets/PrototypingScenes/1/PlayerWorldline.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using SpecialRelativity;
+
+[CreateAssetMenu(fileName = "PlayerWorldline", menuName = "Scriptable Objects/PlayerWorldline")]
+public class PlayerWorldline : ScriptableObject
+{
+    WorldLine Worldline;
+    PhaseSpace PhaseSpace;
+    
+}

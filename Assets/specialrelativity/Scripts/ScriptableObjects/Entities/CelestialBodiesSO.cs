a9555089fe318d2f6eed3e10d90f3a7758cbbdd4
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-27 23:31:28 +0100

    remove prefabname

--- Assets/specialrelativity/Scripts/ScriptableObjects/Entities/CelestialBodiesSO.cs
+++ Assets/specialrelativity/Scripts/ScriptableObjects/Entities/CelestialBodiesSO.cs
@@ -4,7 +4,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CelestialBodies", menuName = "Scriptable Objects/CelestialBodies")]
 public class CelestialBodiesSO : ScriptableObject
 {
-    public string prefabName;
     public int numberOfBodiesToCreate;
-
 }

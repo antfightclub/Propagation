3b4cc1ccde6ad6cc2034a38f413773fedae092e7
monapink <91607167+antfightclub@users.noreply.github.com>
2024-10-27 19:31:03 +0100

    celestial body base class

--- Assets/specialrelativity/Scripts/Entity/Bodies/CelestialBody.cs
+++ Assets/specialrelativity/Scripts/Entity/Bodies/CelestialBody.cs
@@ -1,6 +1,43 @@
 using UnityEngine;
-
-public class CelestialBody
+namespace SpecialRelativity
 {
-    
+    public class CelestialBody
+    {
+        private Vector4D _position;
+        private double _radius;
+        // MORE THINGS!!! because it will actually have a worldline...
+        // I may need to make a "cyclical worldline" data structure as well
+        public Vector4D Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+            }
+        }
+        public double Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value;
+            }
+        }
+        public CelestialBody(Vector4D position, double radius)
+        {
+            this.Position = position;
+            this.Radius = radius;
+        }
+        public void GetRadiusAsMeters(out double radius)
+        {
+            radius = this.Radius * Constants.c;
+        }
+
+    }
 }
--- Assets/specialrelativity/Scripts/Entity/Bodies/Star.cs
+++ Assets/specialrelativity/Scripts/Entity/Bodies/Star.cs
@@ -3,44 +3,10 @@ using UnityEngine;
 
 namespace SpecialRelativity.Entity
 {
-    public class Star
+    public class Star : CelestialBody
     {
-        private Vector4D _position;
-        private double _radius;
+        public Star(Vector4D position, double radius) : base(position, radius) { }
 
-        public Vector4D Position
-        {
-            get 
-            { 
-                return _position; 
-            }
-            set 
-            { 
-                _position = value; 
-            }
-        }
-        public double Radius
-        {
-            get
-            {
-                return _radius;
-            }
-            set
-            {
-                _radius = value;
-            }
-        }
-
-        public Star(Vector4D position, double radius)
-        {
-            this.Position = position;
-            this.Radius = radius;
-        }
-
-        public void GetRadiusAsMeters(out double radius)
-        {
-            radius = this.Radius * Constants.c;
-        }
     }
 
 }
